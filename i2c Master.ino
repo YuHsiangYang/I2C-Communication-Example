a91411a74483f19db6ab57af04bfe55e223448ae
YuHsiangYang <77726213+YuHsiangYang@users.noreply.github.com>
2022-03-07 11:03:57 +0800

    Update the last line of the code

--- i2c Master.ino
+++ i2c Master.ino
@@ -6,5 +6,5 @@ void setup(){
 void loop(){
     Wire.beginTransmission(1);//Put the address of the slave.
     Wire.write("Hello World");//Write data to slave
-    Wire.endTransmission();
+    Wire.endTransmission();//End transmission
 }
